1b3ea42682117d90eca5a6153f620d0f147aaa10
jiango <jrj444@foxmail.com>
2020-07-14 16:49:04 +0800

    完成 ButtonGroup 组件的创建

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import Button from "./Button";
 import Icon from "./Icon";
+import ButtonGroup from "./ButtonGroup";
 
 Vue.component("c-button", Button);
 Vue.component("c-icon", Icon);
+Vue.component("c-button-group", ButtonGroup);
 
 new Vue({
   el: "#app",
