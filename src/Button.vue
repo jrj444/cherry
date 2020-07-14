bd192c053438038b08d466ee2bcff8b0253c2984
jiango <jrj444@foxmail.com>
2020-07-14 14:53:42 +0800

    添加 Button 的 icon 和 iconPosition 外部属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,10 +1,27 @@
 <template>
-  <button class="c-button">按钮</button>
+  <button class="c-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
-    name: "Button"
+    name: "Button",
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: "left",
+        validator(value) {
+          return value === "left" || value === "right";
+        }
+      }
+    }
   };
 </script>
 
@@ -16,6 +33,10 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    vertical-align: middle;
 
     &:hover {
       border-color: var(--border-color-hover);
@@ -27,6 +48,37 @@
 
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      width: 1em;
+      height: 1em;
+      vertical-align: -0.15em;
+      fill: currentColor;
+      overflow: hidden;
+    }
+
+    &.icon-left {
+      > .icon {
+        order: 1;
+        margin-right: .3em;
+      }
+
+      > .content {
+        order: 2;
+      }
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .3em;
+      }
+
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
