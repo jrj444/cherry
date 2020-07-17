7afec98c99f551e5056ab45c104fa9faaea5b6b0
jrj444 <jrj444@foxmail.com>
2020-07-17 14:47:11 +0800

    重构 Row、Col, 添加 align 对齐方式

--- src/Col.vue
+++ src/Col.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="col" :class="[span && `span-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-    <div style="border: 1px solid red; height: 100px;">
-      <slot></slot>
-    </div>
-
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -23,6 +19,20 @@
       return {
         gutter: 0
       };
+    },
+    computed: {
+      colClass() {
+        return [
+          this.span && `span-${this.span}`,
+          this.offset && `offset-${this.offset}`
+        ];
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + "px",
+          paddingRight: this.gutter / 2 + "px"
+        };
+      }
     }
   };
 </script>
