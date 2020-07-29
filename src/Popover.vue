8ad0bf48e47ae08d137475524a3ae9ffaddf82e8
jrj444 <jrj444@foxmail.com>
2020-07-29 11:39:19 +0800

    添加 Popover 四个方向弹出样式

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ch-popover" ref="popover" @click="onClick">
-    <div class="content" ref="content" v-if="visible">
+    <div class="content" ref="content" v-if="visible" :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
     </div>
     <span ref="trigger">
@@ -17,15 +17,41 @@
         visible: false
       };
     },
+    props: {
+      position: {
+        type: String,
+        default: "top",
+        validator(value) {
+          return ["top", "bottom", "left", "right"].indexOf(value) >= 0;
+        }
+      }
+    },
     methods: {
       locateContent() {
-        document.body.appendChild(this.$refs.content);
-        let {top, left} = this.$refs.trigger.getBoundingClientRect();
-        this.$refs.content.style.left = left + window.scrollX + "px";
-        this.$refs.content.style.top = top + window.scrollY + "px";
+        const {content, trigger} = this.$refs;
+        document.body.appendChild(content);
+        let {top, height, width, left} = trigger.getBoundingClientRect();
+        if (this.position === "top") {
+          content.style.left = left + window.scrollX + "px";
+          content.style.top = top + window.scrollY + "px";
+        } else if (this.position === "bottom") {
+          content.style.left = left + window.scrollX + "px";
+          content.style.top = top + height + window.scrollY + "px";
+        } else if (this.position === "left") {
+          let {height: popHeight} = content.getBoundingClientRect();
+          content.style.left = left + window.scrollX + "px";
+          content.style.top = top + window.scrollY + (height - popHeight) / 2 + "px";
+        } else if (this.position === "right") {
+          let {height: popHeight} = content.getBoundingClientRect();
+          content.style.left = left + window.scrollX + width + "px";
+          content.style.top = top + window.scrollY + (height - popHeight) / 2 + "px";
+        }
       },
       onClickDocument(e) {
         if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+          return;
+        }
+        if (this.$refs.content && (this.$refs.content === e.target || this.$refs.content.contains(e.target))) {
           return;
         }
         this.close();
@@ -55,15 +81,111 @@
 </script>
 
 <style lang="scss" scoped>
+  $border-color: #ebeef5;
+
   .ch-popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
+
+    > span {
+      display: inline-block;
+    }
   }
 
   .content {
     position: absolute;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    transform: translateY(-150%);
+    border: 1px solid $border-color;
+    border-radius: 4px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+    background: #fff;
+    padding: .5em 1em;
+    max-width: 20em;
+    word-break: break-all;
+
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 6px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+    }
+
+    &.position-top {
+      transform: translateY(-100%);
+      margin-top: -12px;
+
+      &::before, &::after {
+        margin-left: -6px;
+      }
+
+      &::before {
+        border-top-color: $border-color;
+        top: 100%;
+      }
+
+      &::after {
+        border-top-color: #fff;
+        top: calc(100% - 1px);
+      }
+    }
+
+    &.position-bottom {
+      margin-top: 12px;
+
+      &::before, &::after {
+        margin-left: -6px;
+      }
+
+      &::before {
+        border-bottom-color: $border-color;
+        bottom: 100%;
+      }
+
+      &::after {
+        border-bottom-color: #fff;
+        bottom: calc(100% - 1px);
+      }
+    }
+
+    &.position-left {
+      transform: translateX(-100%);
+      margin-left: -12px;
+
+      &::before, &::after {
+        transform: translateY(-50%);
+        top: 50%;
+      }
+
+      &::before {
+        border-left-color: $border-color;
+        left: 100%;
+      }
+
+      &::after {
+        border-left-color: #fff;
+        left: calc(100% - 1px);
+      }
+    }
+
+    &.position-right {
+      margin-left: 12px;
+
+      &::before, &::after {
+        transform: translateY(-50%);
+        top: 50%;
+      }
+
+      &::before {
+        border-right-color: $border-color;
+        right: 100%;
+      }
+
+      &::after {
+        border-right-color: #fff;
+        right: calc(100% - 1px);
+      }
+    }
   }
 </style>
