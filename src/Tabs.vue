c118dcfd8c071f65c706b89e8e18cf7e6e00f48e
jrj444 <jrj444@foxmail.com>
2020-07-29 09:55:57 +0800

    优化代码结构

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -32,19 +32,27 @@
         eventBus: this.eventBus
       };
     },
-    mounted() {
-      if (this.$children.length === 0) {
-        console && console.warn && console.warn("tabs 的子组件应该是 tabs-head 和 tabs-body");
-      }
-      this.$children.forEach((vm) => {
-        if (vm.$options.name === "TabsHead") {
-          vm.$children.forEach((childVm) => {
-            if (childVm.$options.name === "TabsItem" && childVm.name === this.selected) {
-              this.eventBus.$emit("update:selected", this.selected, childVm);
-            }
-          });
+    methods: {
+      checkChildren() {
+        if (this.$children.length === 0) {
+          console && console.warn && console.warn("tabs 的子组件应该是 tabs-head 和 tabs-body");
         }
-      });
+      },
+      selectTab() {
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === "TabsHead") {
+            vm.$children.forEach((childVm) => {
+              if (childVm.$options.name === "TabsItem" && childVm.name === this.selected) {
+                this.eventBus.$emit("update:selected", this.selected, childVm);
+              }
+            });
+          }
+        });
+      }
+    },
+    mounted() {
+      this.checkChildren();
+      this.selectTab();
     }
   };
 </script>
