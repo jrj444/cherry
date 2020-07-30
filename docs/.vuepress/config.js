881c31b776ed651a215f4ce6e4a2e2506120d59d
jrj444 <jrj444@foxmail.com>
2020-07-30 21:39:21 +0800

    添加 vuePress 生成静态网页

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: "Cherry-UI",
+  description: "一个 Vue UI 组件库",
+  themeConfig: {
+    nav: [
+      {text: "Home", link: "/"}
+    ],
+    sidebar: [
+      {
+        title: '快速开始',
+        children:[
+          '/introduction/',
+          '/getting-started/'
+        ]
+      },
+      {
+        title: '组件',
+        children: [
+          '/components/Button'
+        ]
+      }
+    ]
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: /cherry.png
+heroText: Cherry-UI
+tagline: 一个用 Vue 写的 UI 组件库
+actionText: 快速上手 →
+actionLink: /introduction/
+---
