58e69f407848ac6d58641f26acb989f496a71e05
jrj444 <jrj444@foxmail.com>
2020-09-13 21:54:41 +0800

    修复 css 样式

--- src/components/button/ButtonGroup.vue
+++ src/components/button/ButtonGroup.vue
@@ -18,7 +18,7 @@
   };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .ch-button-group {
     display: inline-flex;
     vertical-align: middle;
@@ -31,13 +31,13 @@
       }
 
       &:first-child {
-        border-top-left-radius: var(--border-radius);
-        border-bottom-left-radius: var(--border-radius);
+        border-top-left-radius: 3px;
+        border-bottom-left-radius: 3px;
       }
 
       &:last-child {
-        border-top-right-radius: var(--border-radius);
-        border-bottom-right-radius: var(--border-radius);
+        border-top-right-radius: 3px;
+        border-bottom-right-radius: 3px;
       }
 
       &:hover {
